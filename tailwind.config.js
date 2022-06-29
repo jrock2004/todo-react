daf10e8c631de2eec8d71d068b7027947544191a
John Costanzo <jrock2004@gmail.com>
2022-06-29 17:26:00 -0400

    chore: adding tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        '398': '398px',
+      },
+      minHeight: {
+        '398': '398px',
+      },
+      minWidth: {
+        '80': '320px',
+        '300': '300px',
+      },
+    },
   },
   plugins: [],
 };
